4aa4f98f7ccea98ed0d9d1a1125a667442077e3c
Ángel Abad <angelabadperez@gmail.com>
2025-06-12 18:46:56 +0200

    feat: Add additional operations

--- index.js
+++ index.js
@@ -41,23 +41,28 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
-    .then(note => { 
-      response.json(note)
+    .then(note => {
+      if (note) { 
+        response.json(note)
+      } else { 
+        response.status(404).end()
+      }
     })
+    .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
-    const body = request.body
+    const { content, important } = request.body
 
-    if(!body.content) { 
+    if(!content) { 
         return response.status(400).json({ error: 'content missing' })
     }
 
     const note = new Note({ 
-        content: body.content,
-        important: body.important || false
+        content: content,
+        important: important || false
     })
     
     note.save().then(savedNote => {
@@ -65,10 +70,28 @@ app.post('/api/notes', (request, response) => {
     })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
-    response.status(204).end()
+app.put('/api/notes/:id', (request, response, next) => { 
+  const { content, important } = request.body
+
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+      }
+
+      note.content = content
+      note.important = important
+
+      return note.save()
+        .then(updatedNote => response.json(updatedNote))
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => response.status(204).end())
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -76,6 +99,18 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT)
