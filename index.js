7659c8bed433db1262f2a2a9746513005779cc9f
Ángel Abad <angelabadperez@gmail.com>
2025-06-18 14:25:47 +0200

    feat: Add validations to Mongoose schema

--- index.js
+++ index.js
@@ -53,21 +53,19 @@ app.get('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const { content, important } = request.body
-
-    if(!content) { 
-        return response.status(400).json({ error: 'content missing' })
-    }
 
     const note = new Note({ 
         content: content,
         important: important || false
     })
     
-    note.save().then(savedNote => {
-      response.json(savedNote)
-    })
+    note.save()
+      .then(savedNote => {
+        response.json(savedNote)
+      })
+      .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => { 
@@ -105,7 +103,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
